5d8372837adc5efa77a39fc36a38f433c3b8bc2e
AnamikaSaini08 <86796513+AnamikaSaini08@users.noreply.github.com>
2022-07-04 12:03:48 +0530

    Update 338. Counting Bits.cpp

--- Bit Manipulation/338. Counting Bits.cpp
+++ Bit Manipulation/338. Counting Bits.cpp
@@ -1,4 +1,5 @@
 //Approach 1 (Brute Force)
+//O(nlogn)
 
 class Solution {
 public:
@@ -21,6 +22,29 @@ public:
             ans.push_back(setBit(i));
         }
         return ans;
+    }
+};
+
+//EFFICIENT APRROACH
+//O(N)
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>v;
+        v.push_back(0);
+        if(n==0)
+            return v;
+        v.push_back(1);
+        
+        for(int i=2 ; i<=n ; i++)
+        {
+            if(i%2)
+                v.push_back(v[i/2] +1);
+            else
+                v.push_back(v[i/2]);
+        }
+        return v;
     }
 };
 
